ec183aabab540bfccc8561cfa845bb2d187cf562
VladanStar <vladancupric@gmail.com>
2023-03-14 21:30:42 +0100

    Add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { environment } from './../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-firebase';
+
+
+  constructor() {
+    console.log(environment.production); // Logs false for development environment
+  }
 }
